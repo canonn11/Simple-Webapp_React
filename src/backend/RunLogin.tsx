2c4a9128ccebbe3fb46e6067c703e2348f31f63e
canonn11 <myungkwanokk@gmail.com>
2022-03-18 03:20:36 +0900

    login function implemented

--- src/backend/RunLogin.tsx
+++ src/backend/RunLogin.tsx
@@ -1,26 +1,34 @@
-import {useQuery, gql} from "@apollo/client"
+import { useQuery, gql } from "@apollo/client";
 
-interface PropTypes{
-    id:string,
-    pw:string
+interface PropTypes {
+  id: string;
+  pw: string;
 }
 
 const GET_USER_INFO = gql`
-query MyQuery {
-    allPeople {
-      nodes {
-        defaultAddress
-        defaultRequest
-      }
+  query personByUserId ($userId :String!){
+    personByUserId(userId : $userId){
+        password
     }
   }
-  
 `;
 
-export default function RunLogin({id,pw} : PropTypes){
-    const {loading, data} = useQuery(GET_USER_INFO);
-    if(!loading){
-        console.log(data.allPeople.nodes);
+export default function RunLogin({ id, pw }: PropTypes) {
+  const { loading, data } = useQuery(GET_USER_INFO, {
+    variables: { userId: id },
+  });
+  if (!loading) {
+    if(data.personByUserId === null){
+        console.log("Login Failed");
     }
-    return (<></>);
+    else if(pw !== data.personByUserId.password){
+        console.log("Login Failed");
+    }
+    else{
+        console.log(`Login Success!
+        id : ${id}
+        password : ${pw}`)
+    }
+  }
+  return <></>;
 }
