7cb62eb6d7106889ad90972be5ac34d773212bfa
canonn11 <myungkwanokk@gmail.com>
2022-03-18 23:35:41 +0900

    Implementing login failed alert using useEffect

--- src/backend/RunLogin.tsx
+++ src/backend/RunLogin.tsx
@@ -1,34 +1,42 @@
+import { useEffect } from "react";
 import { useQuery, gql } from "@apollo/client";
 
 interface PropTypes {
   id: string;
   pw: string;
+  setRunLogin: (flag: boolean) => void;
+  setFailedAlarm : (flag : boolean) => void;
 }
 
 const GET_USER_INFO = gql`
-  query personByUserId ($userId :String!){
-    personByUserId(userId : $userId){
-        password
+  query personByUserId($userId: String!) {
+    personByUserId(userId: $userId) {
+      password
     }
   }
 `;
 
-export default function RunLogin({ id, pw }: PropTypes) {
+export default function RunLogin({ id, pw, setRunLogin, setFailedAlarm }: PropTypes) {
   const { loading, data } = useQuery(GET_USER_INFO, {
     variables: { userId: id },
   });
-  if (!loading) {
-    if(data.personByUserId === null){
-        console.log("Login Failed");
-    }
-    else if(pw !== data.personByUserId.password){
-        console.log("Login Failed");
-    }
-    else{
-        console.log(`Login Success!
-        id : ${id}
-        password : ${pw}`)
-    }
-  }
+  useEffect (() =>{
+    if (!loading) {
+        if (data.personByUserId === null) {
+          //일치하는 ID가 없을때 로그인실패 알람 띄우고 RunLogin 꺼지게
+          setFailedAlarm(true);
+          setRunLogin(false);
+        } else if (pw !== data.personByUserId.password) {
+          //비밀번호 틀렸을 때 로그인실패 알람 띄우고 RunLogin 꺼지게
+          setFailedAlarm(true);
+          setRunLogin(false);
+        } else {
+          console.log(`Login Success!
+            id : ${id}
+            password : ${pw}`);
+        }
+      }
+  } )
+  
   return <></>;
 }
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -10,6 +10,7 @@ export default function Login() {
   const [id, setID] = useState("");
   const [pw, setPW] = useState("");
   const [runLogin, setRunLogin] = useState(false);
+  const [failedAlarm, setFailedAlarm] = useState(false);
   const login = () => {
     setRunLogin(true);
   }
@@ -21,8 +22,9 @@ export default function Login() {
   } else {
     return (
     <>
+    {failedAlarm && <h1>로그인 실패</h1>}
     <LoginMain id={id} pw={pw} setID={setID} setPW={setPW} login = {login} />
-    {runLogin ? <RunLogin id = {id} pw = {pw}/> : <></>}
+    {runLogin && <RunLogin id = {id} pw = {pw} setRunLogin = {setRunLogin} setFailedAlarm = {setFailedAlarm}/>}
     </>
     )
   }
