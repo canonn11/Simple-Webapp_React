301863c36e888d95dee5ebd90c4a7f346468a063
canonn11 <myungkwanokk@gmail.com>
2022-03-25 03:36:56 +0900

    Applying bootstrap to joinroute

--- src/routes/Join.tsx
+++ src/routes/Join.tsx
@@ -1,11 +1,50 @@
 //회원가입을 담당하는 라우트
 
 import Footer from "../components/Footer";
+import "./Join.css";
 
 export default function Join() {
   return (
     <>
-      <h1>Join</h1>
+      <div className="joinMain fullsize">
+        <div className="IdPasswordAddress">
+          <div className="id form-floating">
+            <input
+              type="text"
+              className="form-control"
+              id="idJoinInput"
+              placeholder="ID"
+            />
+            <label htmlFor="idJoinInput">ID</label>
+          </div>
+          <div className="pw form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="pwJoinInput"
+              placeholder="Password"
+            />
+            <label htmlFor="pwJoinInput">PASSWORD</label>
+          </div>
+          <div className="addr form-floating">
+            <input
+              type="text"
+              className="form-control"
+              id="addrJoinInput"
+              placeholder="Address"
+            />
+            <label htmlFor="addrJoinInput">Address</label>
+          </div>
+          <div className="buttonContainer">
+            <div className="d-grid gap-2">
+              <button className="loginbutton btn btn-info" type="button">
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
